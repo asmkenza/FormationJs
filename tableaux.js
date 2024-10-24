050b79eaf92279bce4d6e356e5ece938cb4c2588
asmkenza <kstudies17studies@gmail.com>
2024-10-24 22:05:20 +0200

    Les tableaux

---
+++ tableaux.js
@@ -0,0 +1,31 @@
+const myArray=['Banane','Pomme','Cerise','Poire',4]
+
+console.log(myArray,"Mon Tableau"); 
+console.log(myArray.length,"La longueur de mon tableau myArray"); 
+console.log(myArray.toString(),"Conversation de mon tableau en chaine de caractère"); 
+console.log(myArray.join("/")); 
+
+
+
+
+const cars=new Array('BMW','Citroen','Renault')
+
+console.log(cars,"Mes Voiture");
+
+let voiture=cars.pop(); 
+console.log(voiture,"La voiture retirée");
+
+let fruit= myArray.push('kiwi');
+console.log(fruit,"ajouter un element Fruits");
+console.log(myArray,"Tableau Fruits");
+console.log(myArray[1]);
+myArray[0]='Papaye';
+console.log(myArray); 
+
+const boys=["Jean","Paul","Jacques"];
+const girls=["Marie","Anne","Sophie"];
+
+
+const myChildren= boys.concat(girls); 
+console.log(myChildren,"My Children");
+
